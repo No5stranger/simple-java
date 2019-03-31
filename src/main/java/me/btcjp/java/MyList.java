d8cf8bb0b51babd3fdaec7566709fbf266db4e33
cjp <No5stranger@gmail.com>
2019-03-31 16:56:41 +0800

    mockito verify

--- src/main/java/me/btcjp/java/MyList.java
+++ src/main/java/me/btcjp/java/MyList.java
@@ -2,7 +2,7 @@ package me.btcjp.java;
 
 import java.util.AbstractList;
 
-public class MyList extends AbstractList {
+public class MyList<E> extends AbstractList {
 
     @Override
     public String get(final int index) {
--- src/test/java/me/btcjp/java/MyListTest.java
+++ src/test/java/me/btcjp/java/MyListTest.java
@@ -2,6 +2,7 @@ package me.btcjp.java;
 
 import org.junit.*;
 import org.mockito.Mockito;
+import org.mockito.InOrder;
 import org.apache.commons.lang3.RandomStringUtils;
 
 public class MyListTest {
@@ -26,5 +27,41 @@ public class MyListTest {
         MyList listMock = Mockito.mock(MyList.class);
         Mockito.when(listMock.add(Mockito.anyString())).thenThrow(IllegalStateException.class);
         listMock.add(RandomStringUtils.randomAlphabetic(6));
+    }
+
+    @Test(expected = IllegalStateException.class)
+    public void testMulitBehavior() {
+        MyList listMock = Mockito.mock(MyList.class);
+        Mockito.when(listMock.add(Mockito.anyString())).thenReturn(false).thenThrow(IllegalStateException.class);
+        boolean added = listMock.add(RandomStringUtils.randomAlphabetic(5));
+        Assert.assertFalse(added);
+
+        listMock.add(Mockito.anyString());
+    }
+
+    public void testVerify() {
+        MyList listMock = Mockito.mock(MyList.class);
+        listMock.size();
+        Mockito.verify(listMock).size();
+        Mockito.verify(listMock, Mockito.times(1)).size();
+        Mockito.verify(listMock, Mockito.atLeastOnce()).size();
+        Mockito.verify(listMock, Mockito.atMost(10)).size();
+
+        listMock.add("abc");
+        Mockito.verify(listMock).add("abc");
+    }
+
+    public void testInteraction() {
+        MyList listMock = Mockito.mock(MyList.class);
+        Mockito.verifyZeroInteractions(listMock);
+
+        listMock.size();
+        listMock.add("abc");
+        listMock.clear();
+
+        InOrder inOrder = Mockito.inOrder(listMock);
+        inOrder.verify(listMock).size();
+        inOrder.verify(listMock).add("abc");
+        inOrder.verify(listMock).clear();
     }
 }
